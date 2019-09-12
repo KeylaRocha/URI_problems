8e5bc09246297709f225749b8461ada921007ca1
Keyla Rocha <keylarocha@hotmail.com.br>
2019-09-11 21:18:50 -0300

    New problem solved

---
+++ Beginner/1866.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int c, n;
+  cin >> c;
+  while(c > 0)
+  {
+    cin >> n;
+    if(n%2 == 0)
+      cout << 0 << endl;
+    else
+      cout << 1 << endl;
+    c--;
+  }
+
+  return 0;
+}
